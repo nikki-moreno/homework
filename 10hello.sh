e5ec5863bc6d42bea6a518ed8607695f6de118d5
nikki-moreno <nimoreno@ucdavis.edu>
2023-01-12 23:44:50 -0800

    initial_commit

---
+++ 10hello.sh
@@ -0,0 +1,8 @@
+### 10hello.sh
+### Nikki Moreno
+### MCB 185
+### 12 January, 2023
+### Purpose: examining shell script
+echo "Hello $USER, your home is $HOME, and your shell is $SHELL"
+ls $HOME/Code/*
+ls $HOME/DATA
