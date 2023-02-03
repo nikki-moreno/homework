ac360450f384800f685cbd32cc14007c1f2f25f1
nikki-moreno <nimoreno@ucdavis.edu>
2023-02-02 22:59:49 -0800

    updated

--- Programs/21codons.py
+++ Programs/21codons.py
@@ -6,6 +6,11 @@
 
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
 
+for frame in range(3):      # for a variable in a specific range of integers
+    for position in range(frame, len(dna) -2, 3):
+        codon = dna[position:position+3]
+        print(codon)
+    print()
 
 """
 python3 21codons.py
