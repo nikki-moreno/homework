2c5432e9eb83c6e79e122bf44ee313bb05358cdb
nikki-moreno <nimoreno@ucdavis.edu>
2023-02-04 15:05:36 -0800

    completed

--- 22sumfac.py
+++ 22sumfac.py
@@ -1,10 +1,32 @@
 # 22sumfac.py
 
+# Nikki Moreno
+# 4 February 2023
+# MCB 185
+
+## Purpose ##
 # Write a program that computes the running sum from 1..n
 # Also, have it compute the factorial of n while you're at it
 # Use the same loop for both calculations
-
 # Note: you may not import math or any other library
+
+
+# setting variables
+n = 5
+x = 1
+print (n, end=" ")
+
+# for loop computing running sum from 1...n
+for i in range(x,n):
+	x = x + (i+1)
+print(x, end=" ")
+
+# for loop computing factorial of n
+n = 5
+x = 1 
+for i in range(x,n+1):
+	x = x * i
+print(x, end=" ")
 
 
 """
