15f90af57ec880c88b8a6dfd8bc1b2451ca2404e
nikki-moreno <nimoreno@ucdavis.edu>
2023-02-05 17:04:00 -0800

    incomplete

--- 27frame.py
+++ 27frame.py
@@ -1,13 +1,24 @@
 # 27frame.py
 
+# Nikki Moreno
+# 5 February 2023
+# MCB 185
+
+## Purpose ##
 # Write a program that prints out the position, frame, and letter of the DNA
-
 # Variation: try coding this with a single loop and nested loops
-
 # Note: use 0-based indexing for position and frame (biology uses 1-based)
 
 dna = 'ATGGCCTTT'
 
+frame = 0
+for position in range(len(dna)):
+	for nucleotides in range(len(dna)):
+		nucleotides = dna[position]
+		while (True):
+			frame = frame + 1
+			if frame > 2: break 
+	print (position, frame, nucleotides)
 
 """
 python3 27frame.py
