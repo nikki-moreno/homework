ea18054cfc31e45b1324af6aa7eb4513a4bacdbb
nikki-moreno <nimoreno@ucdavis.edu>
2023-03-02 12:14:33 -0800

    completed

--- 31entropy.py
+++ 31entropy.py
@@ -1,7 +1,7 @@
 # 31entropy.py
 
 # Nikki Moreno
-# 8 February 2023 & 14 Ferbuary 2023
+# 8 February 2023 - 2 March 2023
 # MCB 185
 
 ## Purpose ##
@@ -13,43 +13,30 @@
 # Note: import math and use the math.log2()
 # Hint: try breaking your program with erroneous input
 
- 
+
+
+# Imports
 import sys
 import math
 
 
-# iteration
-vals = []					# Empty list.
-for i in sys.argv[1:]:		# For positions in system argument,
-	vals.append(float(i)) 	# add the positions past [:1] to the empty list.	
-	if sum(vals) == 1:		# Check to see if the list sum = 1
-		I = -math.log2(vals[i]) + I		#mathisclose (line 25)
-print(vals)
+# Empty list and initial variables 
+vals = []				
+H = 0
 
+# Make sure probabilities sum to approximately 1.
+for i in sys.argv[1:]:		
+	vals.append(float(i)) 	
+assert(math.isclose(sum(vals), 1.0, abs_tol=0.01))
 
+# Entropy calculation
+for x in vals:				
+	H = (-x * math.log2(x)) + H
+print(f'{H:.3f}')
 
-"""
-i = 0
-i = sys.argv([1:]) 
-H = -sum(pi * log(pi))
-"""
 
 
 """
 python3 31entropy.py 0.1 0.2 0.3 0.4
 1.846
 """
-
-
-
-## Shanon Entropy ##
-# Information Theory - I(m) =  -log2P(m)
-# Entropy - sum up all information contents and weight them by how often they occur
-# Entropy = 0 when its not random, 100% predictable
-# calculate the entropy of a frequency distribution
-# for loop = sigma
-# (0.1 * log2 0.1) + (0.2*log0.2)
-# for p in ps
-# H-=pmath.log2p
-# for i in range(len(p))
-# H += P[i]*log2p[i]
